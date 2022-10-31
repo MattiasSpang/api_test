0cd246e5d4c73467c8b2486fd45aea7d754ce142
MattiasSpang <spangberg.mattias@gmail.com>
2022-10-31 07:51:20 +0100

    Initial Commit

---
+++ api_test/api_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_testApp.swift
+//  api_test
+//
+//  Created by Mattias Spångberg on 2022-10-31.
+//
+
+import SwiftUI
+
+@main
+struct api_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
